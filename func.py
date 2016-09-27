a1b6b62a0ef61332987ca7e62fbae0cf50faff23
conformist-mw <conformist.mw@gmail.com>
2016-09-27 17:21:18 +0300

    implements basic functions

--- func.py
+++ func.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 # coding: utf-8
-# from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup
 import re
 import requests
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0'}
 
 
 def get_phone(url):
@@ -13,5 +15,26 @@ def get_phone(url):
     return [s for s in phones if s != ' ']
 
 
-print(get_phone('http://www.olx.ua/obyavlenie/amortizatory-izh-planeta-yupiter-ID6P5lH.html#e50e5c6917;promoted'))
-print(get_phone('www.olx.ua/obyavlenie/usilitel-na-tda7294-2-x-150-vt-mostovoy-IDlRCzg.html'))
+def get_data(url):
+    ''' collect all data in dict '''
+    html = requests.get(url, headers=headers)
+    soup = BeautifulSoup(html.content, 'lxml')
+    data = {}
+    data['title'] = soup.find('h1').text.strip()
+    data['price'] = soup.find('strong', class_='xxxx-large').string
+    data['address'] = soup.find('strong', class_='c2b small').string.strip()
+    adr = soup.find('small', class_='c62').text.split(',')
+    data['time'] = re.findall(r'[0-9]{2}:[0-9]{2}', adr[0])[0]
+    data['date'] = adr[1].strip()
+    data['ad_num'] = re.findall(r'[0-9]+', adr[2])[0]
+    a = soup.find(class_='userdetails').findChildren('span')
+    x, y = [s.text for s in a[:2]]
+    data['name'] = x + ' ' + y
+    tds = soup.find('table', class_='details').find_all('table', class_='item')
+    data['tags'] = [' '.join(t.text.split()) for t in tds]
+    data['content'] = soup.find('div', id='textContent').p.text.strip()
+    imgs = soup.findAll('img', class_='bigImage')
+    data['img'] = [i.get('src') for i in imgs]
+    data['phone'] = get_phone(url)
+    data['link'] = url
+    return data
--- script.py
+++ script.py
@@ -1,5 +1,25 @@
 #!/usr/bin/python3
 # coding: utf-8
-# from bs4 import BeautifulSoup
-# import re
-# import requests
+from bs4 import BeautifulSoup
+import requests
+import csv
+from func import *
+import sys
+
+header = ['link', 'title', 'price', 'name', 'phone', 'time',
+          'date', 'address', 'tags', 'content', 'ad_num', 'img']
+html = requests.get(sys.argv[0])
+soup = BeautifulSoup(html.content, 'lxml')
+links = soup.findAll('td', class_='offer')
+lst = []
+for link in links:
+    a = link.find('a')
+    if a:
+        lst.append(a.get('href'))
+with open('olx.csv', 'w') as f:
+    w = csv.writer(f)
+    w.writerow(header)
+    for link in lst:
+        d = get_data(link)
+        wlist = [d[i] for i in header]
+        w.writerow(wlist)
